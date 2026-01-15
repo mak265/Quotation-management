605fabd6ef5465626054e7059d13e24143474da9
reymark6 <rees.mariano.au@phinmaed.com>
2026-01-15 14:00:56 +0800

    Initial commit

---
+++ src/mainwindow.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle("Quotation Qt");
+    resize(800, 600);
+}
+
+MainWindow::~MainWindow()
+{
+}
